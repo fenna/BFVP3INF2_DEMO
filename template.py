032e551bb7cfde32a73592691f911b0f2a7f8ee9
Fenna Feenstra <flen@mac.com>
2020-11-17 13:04:21 +0100

    demo scripts week 1

---
+++ template.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+
+"""
+description of the program
+"""
+
+
+__author__ = "my name"
+
+
+import sys
+
+
+def main(args):
+    return 0
+
+
+if __name__ == "__main__":
+    exitcode = main(sys.argv)
+    sys.exit(exitcode)

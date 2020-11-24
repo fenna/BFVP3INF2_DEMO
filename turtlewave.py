2044d732fcb9c3a168405642ccfeb9ff44b95c7a
Fenna Feenstra <flen@mac.com>
2020-11-24 14:20:50 +0100

    demo scripts week 2

--- turtlewave.py
+++ turtlewave.py
@@ -6,10 +6,14 @@ program shows turtle following sinuswave
 
 __author__ = "F.Feenstra"
 
+## CODE
+
+#IMPORTS
 import numpy as np
 import sys
 import turtle
 
+#CONSTANTS
 MARGE = 10
 SCREEN = turtle.Screen()
 SAMPLE_RATE = int(SCREEN.window_width()/2 -MARGE) - int(MARGE - SCREEN.window_width()/2)
@@ -17,6 +21,7 @@ AMPLITUDE = SCREEN.window_height()/4
 PHASE = 0
 FREQUENCY = 2
 
+#FUNCTIONS
 
 def comp_y(t):
     """compute the y value of the sin wave at the for a specific sample"""
@@ -34,17 +39,33 @@ def comp_amplitude(x):
 
 def turtle_wave(start, y):
     """Draw a turtle and move it over the sinewave, starting at _start_"""
-    dude = turtle.Turtle(shape="turtle", visible = False)
+    dude = turtle.Turtle(shape="turtle", visible=False)
     dude.penup()
     i = start
     for j in range(0, SAMPLE_RATE):
         pos = dude.pos()
-        dude.setheading(np.arctan2(y[j]-pos[1], i-pos[0]) * 180 / np.pi) 
+        dude.setheading(np.arctan2(y[j]-pos[1], i-pos[0]) * 180 / np.pi)
         dude.goto(i, y[j])
         if i == start:
             dude.pendown()
             dude.showturtle()
         i +=1
+    dude.hideturtle()
+    return
+
+def turtle_wave_back(end, y):
+    """Draw a turtle and move it backwards over the sinewave, from _end_ to _start_"""
+    dude = turtle.Turtle(shape="turtle", visible=False)
+    dude.penup()
+    i = end
+    for j in range(SAMPLE_RATE-1, 0, -1):
+        pos = dude.pos()
+        dude.setheading(np.arctan2(y[j]-pos[1], i-pos[0]) * 180 / np.pi)
+        dude.goto(i, y[j])
+        if i == end:
+            dude.pendown()
+            dude.showturtle()
+        i -=1
     return
 
 
@@ -55,6 +76,7 @@ def main():
     x = np.arange(start, end) # the points on the x axis for plotting
     y = comp_amplitude(x) # the points on the y axis for plotting
     turtle_wave(start, y)
+    turtle_wave_back(end, y)
     return 0
 
 
